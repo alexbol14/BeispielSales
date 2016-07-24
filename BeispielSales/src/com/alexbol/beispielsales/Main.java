41eaaf50cac4558338fdfdfdee669e9a18e643fa
Alexander <alexbol1214@gmail.com>
2016-07-24 18:11:54 +0200

    Initial checkin of Example Sales.

---
+++ BeispielSales/src/com/alexbol/beispielsales/Main.java
@@ -0,0 +1,18 @@
+package com.alexbol.beispielsales;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		
+		
+		displayGreeting();
+		data.display();
+
+	}
+
+	public static void displayGreeting() {
+		System.out.println("Hallo");
+	}
+}
---
+++ BeispielSales/src/com/alexbol/beispielsales/SalesData.java
@@ -0,0 +1,10 @@
+package com.alexbol.beispielsales;
+
+public class SalesData {
+	int data[] = { 0, 4, 22 };
+
+	public void display() {
+		System.out.println("ya esta");
+	}
+
+}
